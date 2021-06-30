fd6eb790eafd0842c342e964f7d190ebbb6b45e0
d4g0 <tocarralero@gmail.com>
2021-06-29 20:00:29 -0400

    og:image

--- pages/index.vue
+++ pages/index.vue
@@ -17,5 +17,74 @@ import Card from "~/components/home/Card.vue";
 import Blendy from "~/components/app/Blendy.vue";
 export default {
   components: { Card, Blendy },
+  head() {
+    return {
+      title: "Dago | Vue Frontend Developer",
+      htmlAttrs: {
+        lang: "en",
+      },
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Web Development Services",
+        },
+        { hid: "og:title", property: "og:title", content: "Railab" },
+        {
+          hid: "og:site_name",
+          property: "og:site_name",
+          content: "Dago | Vue Frontend Developer",
+        },
+        {
+          hid: "og:description",
+          property: "og:description",
+          content: "Web Development Services",
+        },
+        { hid: "og:type", property: "og:type", content: "website" },
+        {
+        hid: 'twitter:card',
+        name: 'twitter:card',
+        content: 'summary_large_image'
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'http://dago-cv.netlify.app/img/dago-web-development-services.jpg'
+      },
+      {
+        hid: 'og:image:secure_url',
+        property: 'og:image:secure_url',
+        content: 'https://dago-cv.netlify.app/img/dago-web-development-services.jpg'
+      },
+      {
+        hid: 'og:image:alt',
+        property: 'og:image:alt',
+        content: 'Dago Web Development Services'
+      },
+      {
+        hid: 'twitter:image',
+        name: 'twitter:image',
+        content: ''
+      }
+      ],
+      link: [
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+      ],
+    };
+  },
 };
 </script>
